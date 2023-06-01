1f043a215b00de7e4b464907191a4b95da5ae17a
Pixulegos <felipe.910@hotmail.com>
2023-06-01 12:48:22 -0500

    Ecuaciones entre vetores

--- Proyecto Integrador/src/Algebra.java
+++ Proyecto Integrador/src/Algebra.java
@@ -41,4 +41,27 @@ public class Algebra extends JFrame {
         pi.setVisible(true);
     }
     
+    //Operaciones entre vectores
+    public int[] sumaV(int[] v1, int[] v2){
+        int[] V = new int[2];
+        V[0] = v1[0]+v2[0];//Suma posiciones en X
+        V[1] = v1[1] + v2[1]; //Suma posiciones en Y
+        return V;
+    }
+    
+    public int[] prodporE(int[] v1, int n){
+        int[] V = new int [2];
+        V[0] = v1[0]*n; //Multiplica las posiciones por el escalar
+        V[1] = v1[1]*n; //Si piden un numero entre 1 y 0 cambie el int por un double 
+        return V;
+    } 
+    
+    public int prodE(int[] v1, int[] v2){
+       return (v1[0]*v2[0+v1[1]+v2[1]]); //Devuelve un entero, multiplica las posiciones y suma el resultado de ambas
+    }
+    
+    //Los otros dos no se mano, vi algibra linial hace mucho
+    //Pal ultimo como es una descomposicion solo grafica dos vectores 
+    // Vector 1 ( X del vector, 0 )  |
+    // Vector 2 ( 0, Y del vector )  |_ _
 }
